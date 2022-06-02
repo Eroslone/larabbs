119eb3ebc1b9a9fc1abdb3d4a28ef213f185ad22
Eroslone <3079758191@qq.com>
2022-06-02 09:24:20 +0000

    分类话题列表

--- resources/views/topics/index.blade.php
+++ resources/views/topics/index.blade.php
@@ -1,11 +1,17 @@
 @extends('layouts.app')
 
-@section('title', '话题列表')
+@section('title', isset($category) ? $category->name : '话题列表')
 
 @section('content')
 
   <div class="row mb-5">
     <div class="col-lg-9 col-md-9 topic-list">
+      {{--定制列表页面模板，用以标示当前所在的分类，以此来与『所有话题列表页面』区分：--}}
+      @if (isset($category))
+        <div class="alert alert-info" role="alert">
+          {{ $category->name }} ：{{ $category->description }}
+        </div>
+      @endif
       <div class="card ">
 
         <div class="card-header bg-transparent">
