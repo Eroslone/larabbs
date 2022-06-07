5b8790031778af6f97ef01d7587034a33bb83643
Eroslone <3079758191@qq.com>
2022-06-07 01:27:27 +0000

    授权作者可以编辑话题

--- app/Policies/TopicPolicy.php
+++ app/Policies/TopicPolicy.php
@@ -9,8 +9,7 @@ class TopicPolicy extends Policy
 {
     public function update(User $user, Topic $topic)
     {
-        // return $topic->user_id == $user->id;
-        return true;
+        return $topic->user_id == $user->id;//只允许作者对话题有编辑权限
     }
 
     public function destroy(User $user, Topic $topic)
