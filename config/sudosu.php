d8296e4337f76b082df7b636882aa1a7175475eb
Eroslone <3079758191@qq.com>
2022-09-29 14:05:09 +0800

    网站底部添加备案号

--- config/sudosu.php
+++ config/sudosu.php
@@ -1,10 +1,8 @@
 <?php
-
+//修改config/sudosu.php，添加域名后缀`com`
 return [
-
     // 允许使用的顶级域名
-    'allowed_tlds' => ['dev', 'local', 'test'],
-
+    'allowed_tlds' => ['dev', 'local', 'app', 'test','com','cn'],
     // 用户模型
     'user_model' => App\Models\User::class
 ];
--- resources/views/layouts/_footer.blade.php
+++ resources/views/layouts/_footer.blade.php
@@ -3,6 +3,9 @@
         <p class="float-start">
             由 <a href="http://weibo.com/u/1837553744?is_hot=1" target="_blank">Summer</a> 设计和编码 <span style="color: #e27575;font-size: 14px;">❤</span>
         </p>
+    <p class="float-start">
+      备案号:<a href="https://beian.miit.gov.cn/" target="_blank">鲁ICP备2022004954号-2</a>
+    </p>
 
         <p class="float-end"><a href="mailto:{{ setting('contact_email') }}">联系我们</a></p>
     </div>
